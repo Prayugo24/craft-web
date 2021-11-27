dfdcb8b195e29171f6ffbc6989a9b10acdfe9308
Prayugo24 <dwi.deni24@gmail.com>
2021-11-27 23:02:49 +0700

    update home

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -6,14 +6,14 @@ class HomeController extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
-		$this->load->model('categorymodels');
+		$this->load->model('CategoryModels');
 		$this->load->helper('form');
 		
 	}
 	public function index()
 	{
 		$params['menu']="home";
-		$params['category']=$this->categorymodels->getAll();	
+		$params['category']=$this->CategoryModels->getAll();	
 		$this->load->view('template_2/nav/header',$params);
 		$this->load->view('template_2/home',$params);
 		$this->load->view('template_2/nav/footer');
